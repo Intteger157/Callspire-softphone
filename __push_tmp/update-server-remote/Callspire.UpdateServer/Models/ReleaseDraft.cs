fb30a8c779bc446588ec8cbd2b469a621baeebdd
unknown <al.shegonov@gmail.com>
2026-03-26 13:12:37 +0300

    Revert "Update update-server (admin UI, release history, draft notes)"
    This reverts commit 50a751ea187da270cfeb6ed8697f98e7c1f85b50.

--- __push_tmp/update-server-remote/Callspire.UpdateServer/Models/ReleaseDraft.cs
+++
@@ -1,9 +0,0 @@
-namespace Callspire.UpdateServer.Models;
-
-public class ReleaseDraft
-{
-    public string? Version { get; set; }
-    public string? Notes { get; set; }
-    public bool Mandatory { get; set; }
-}
-
